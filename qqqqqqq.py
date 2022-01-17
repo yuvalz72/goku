cfbde53552a7a74350dad3434ac8cc3ffb8f41d8
yuvalz72 <97913727+yuvalz72@users.noreply.github.com>
2022-01-17 21:33:50 +0200

    Add files via upload

---
+++ qqqqqqq.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import streamlit as st
+
+#header = st.beta_container()
+#dataset = st.beta_container()
+#features = st.beta_container()
+#modelTraining = st.beta_container()
+st.title ("tsur is gay!")
+st.header("**noam is gay too**")
+st.subheader (" hello this is the offical fan page for the one and only, the chosen one, the wonder boy, harry potter.")
+st.header("naa JK this is the offical fan page of kakarot AKA **goku**")  
+    
+              
+        
